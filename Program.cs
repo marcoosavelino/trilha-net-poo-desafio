2da4743bac489765b20ddeea1a176b849c53d7ac
marcoosavelino <71901248@aluno.faculdadecotemig.br>
2022-09-28 17:47:49 -0300

    Desafio de Projeto - Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("498787", "8 Plus", "987654321", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("993155", "G50", "123456789", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
